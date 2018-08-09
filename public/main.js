45652de80754af73b9bfaae67a75a39572f0b78b
unknown <cbejar93@gmail.com>
2018-08-09 15:27:51 -0400

    got it all nice and ready

--- public/main.js
+++ public/main.js
@@ -13,9 +13,9 @@
             let total = 0;
             for (var i=0; i< ratings.length; i++){
                let what = ratings.length;
-               console.log(what);
+            //    console.log(what);
                 total= ratings[i].range + total
-                console.log(total);
+                // console.log(total);
                 if (i+1 === what){
                     let avg = total/what;
                     let final =Math.ceil(avg);
@@ -33,12 +33,38 @@
 
   $(".btn").click(function(){
       const rangeValue = $("#test5").val();
-      console.log(rangeValue);
+    //   console.log(rangeValue);
       $.ajax({
         type: 'POST',
         data: {range: rangeValue},
-        url: "/rate",
-        success: console.log("You have made it!")
-    })
+        url: "/rate"
+        // success: console.log("You have made it!")
+    });
+
+    $.ajax({
+        dataType: "json",
+        url: "/ratings",
+        success: function(data){
+            const ratings = data;
+            let total = 0;
+            for (var i=0; i< ratings.length; i++){
+               let what = ratings.length;
+            //    console.log(what);
+                total= ratings[i].range + total
+                // console.log(total);
+                if (i+1 === what){
+                    let avg = total/what;
+                    let final =Math.ceil(avg);
+                    $("#here").text(final);
+                }
+                
+                
+            }
+            
+        }
+
+    });
+
+
   })
 
