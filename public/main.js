889bdf664de9ecd1db5600e7bc1bc5e3fee81960
unknown <cbejar93@gmail.com>
2019-09-17 22:47:26 -0700

    minor mobile changes

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 
 
   $(document).ready(function(){
-    $('.parallax').parallax();
+   
     $('.fixed-action-btn').floatingActionButton(); 
 
     var typed = new Typed('#typed', {
@@ -105,47 +105,6 @@
     pathEl.setAttribute("stroke-dashoffset", offset);
   }
   
-  // basicTimeline
-  //   .add({
-  //     targets: ".text",
-  //     duration: 1,
-  //     opacity: "0"
-  //   })
-  //   .add({
-  //     targets: ".button",
-  //     duration: 1300,
-  //     height: 10,
-  //     width: 300,
-  //     backgroundColor: "#2B2D2F",
-  //     border: "0",
-  //     borderRadius: 100
-  //   })
-  //   .add({
-  //     targets: ".progress-bar",
-  //     duration: 2000,
-  //     width: 300,
-  //     easing: "linear"
-  //   })
-  //   .add({
-  //     targets: ".button",
-  //     width: 0,
-  //     duration: 1
-  //   })
-  //   .add({
-  //     targets: ".progress-bar",
-  //     width: 80,
-  //     height: 80,
-  //     delay: 500,
-  //     duration: 750,
-  //     borderRadius: 80,
-  //     backgroundColor: "#71DFBE"
-  //   })
-  //   .add({
-  //     targets: pathEl,
-  //     strokeDashoffset: [offset, 0],
-  //     duration: 200,
-  //     easing: "easeInOutSine"
-  //   });
   
   $(".button").click(function() {
     basicTimeline.play();
